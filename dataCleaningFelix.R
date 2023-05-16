45a1a4e43f6d6e7adfe471804775856e9c70e6ee
olav-cor <olav.cor@gmail.com>
2023-05-16 21:03:26 +0200

    commit

---
+++ dataCleaningFelix.R
@@ -0,0 +1,24 @@
+# Felix - Data Cleaning - 13.05.2023
+
+
+library(utils)
+
+# Read in the weather data file
+weather <- read.csv('data/original/weather.csv')
+
+# Read in the kiwo data file
+kiwo <- read.csv('data/original/kiwo.csv')
+
+# Add a new column "KiWo" to the weather data frame, initialized with 0
+weather$KiWo <- 0
+
+# Loop through each row in the weather data frame and check if the date exists in the kiwo data frame
+for (i in 1:nrow(weather)) {
+  date <- as.character(weather[i, "Datum"])
+  if (date %in% kiwo$Datum) {
+    weather[i, "KiWo"] <- 1
+  }
+}
+
+# Write the updated weather data frame to a new CSV file
+write.csv(weather, "data/updated_file.csv", row.names = FALSE)
