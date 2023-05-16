4b2ceaa0ba86b17a6030e162f22647fd29f61469
flx247 <spam@wehkamp.de>
2023-05-16 20:52:45 +0200

    neu

---
+++ dataPrep/ScriptSchulferienAlex.R
@@ -0,0 +1,29 @@
+# Installiere und lade das "lubridate" Paket für die Arbeit mit Datumsangaben
+install.packages("lubridate")
+library(lubridate)
+
+# Dateinamen und Spaltennummern anpassen
+csv_datei <- "ferienRaw.csv"
+start_spalte <- 1
+ende_spalte <- 2
+
+# Lese die CSV-Datei ein
+daten <- read.csv(csv_datei, header = TRUE)
+
+# Konvertiere die Spalten in Datumsangaben
+start_datum <- dmy(daten[, start_spalte])
+ende_datum <- dmy(daten[, ende_spalte])
+
+# Konvertiere das Datum in das Format "yyyy-mm-dd"
+start_datum <- format(start_datum, "%Y-%m-%d")
+ende_datum <- format(ende_datum, "%Y-%m-%d")
+
+# Erzeuge einen Vektor mit allen Tagen im Zeitraum
+alle_tage <- seq.Date(as.Date(start_datum), as.Date(ende_datum), by = "day")
+
+# Speichere die Tage in einer neuen CSV-Datei
+ausgabedatei <- "SchulferienSH.csv"
+write.csv(alle_tage, file = ausgabedatei, row.names = FALSE)
+
+# Gib eine Bestätigung aus
+cat("Die Tage wurden erfolgreich in", ausgabedatei, "gespeichert.\n")
