4e0d664248b02c22dad9c9ef8d1236a78518f32d
flx247 <spam@wehkamp.de>
2023-05-23 19:29:22 +0200

    Resorting Datapoints

---
+++ dataPrep/duplicateRemoval.py
@@ -0,0 +1,44 @@
+import csv
+from datetime import datetime, timedelta
+
+
+i = 0 #counter for early break
+data = []
+finalRow = {}
+transferDict = {}
+lastDate = ""
+valuesToTransfer = ["Datum", "KielerWoche", "Bewoelkung", "Temperatur", "Windgeschwindigkeit", "Wettercode", "Wochentag", "FerienSH"]
+
+
+with open('../data/data_combined_sorted_cropped.csv', newline='') as csvfile: # insert ordered dataset!! (chronolgically)
+    reader = csv.DictReader(csvfile, delimiter=';')
+    for row in reader: 
+        if row['Datum'] != lastDate: 
+            if i > 5: # 6 ist Anzahl der Produktgruppen               
+                data.append({**finalRow, **transferDict})
+                finalRow = {}       #reset row
+                transferDict = {}
+
+            #run once!!
+            for value in valuesToTransfer:
+                finalRow[value] = row[value]
+                
+        if row["Warengruppe"] != "NA":
+            transferDict[row["Warengruppe"]] = row["Umsatz"]
+        print(transferDict)
+        #break
+        i += 1
+
+        lastDate = row['Datum']
+            
+# print(data)
+
+with open('output.csv', 'w', newline='') as csvfile2:
+    writer = csv.DictWriter(csvfile2, fieldnames=valuesToTransfer + ['Brot', 'Brötchen', 'Kuchen', 'Croissant', 'Konditorei', 'Saisonbrot'])
+
+    # Write the header row
+    writer.writeheader()
+
+    # Write the data rows
+    for row in data:
+        writer.writerow(row)
